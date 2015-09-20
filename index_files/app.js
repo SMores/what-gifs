4e99e8d85bcd0ee906de648d6d80be8b84ed8557
Shane Moore <shanewmoore@gmail.com>
2015-09-20 02:32:48 -0400

    Tried to get multiple tries working, not 100% convinced it works

--- index_files/app.js
+++ index_files/app.js
@@ -28,6 +28,64 @@ function string2ArrayBuffer(string, callback) {
     f.readAsArrayBuffer(blob);
 }
 
+function processVideo(video, stream) {
+  $('#sharing').css({display: 'block'});
+  recordRTC.stopRecording(function(videoWebURL) {
+    $('#record').html('Try Again');
+    $('#record').off('click');
+    $('#record').click(function() {
+      $('#reset').remove();
+      video.src = URL.createObjectURL(stream);
+      video.controls = false;
+      video.loop = false;
+      record(video, stream);
+    });
+    $('#controls').append($('<button id="reset">Pick Another Window</button>'));
+    $('#reset').click(function(e) {
+      video.scr = '';
+      video.controls = false;
+      video.loop = false;
+      $(this).remove();
+      $('#record').html('Record');
+      $('#record').off('click');
+      $('#record').click(function() {
+        record(video, stream);
+      });
+      selectWindow(e);
+    });
+    var recordedBlob = recordRTC.getBlob();
+    chrome.runtime.sendMessage({
+      'action': 'crop',
+      'url': videoWebURL,
+      'width': 1920,
+      'height': 1200,
+      'horizontal': 0,
+      'vertical': 0,
+    }, function(croppedURL) {
+      video.src = croppedURL;
+      video.controls = true;
+      video.loop = true;
+      
+    });
+    
+    // $('body').append($('<img/>'));
+    // $('img').src = videoWebURL;
+
+    recordRTC.getDataURL(function(dataURL) { });
+  });
+}
+
+function record(video, stream) {
+  options.video = video;
+  recordRTC = RecordRTC(stream, options);
+  recordRTC.startRecording();
+  $('#record').html('Stop');
+  $('#record').off('click');
+  $('#record').click(function () {
+    processVideo(video, stream);
+  });
+}
+
 function gotStream(stream) {
   $('#start').remove();
   console.log("Received local stream");
@@ -38,35 +96,7 @@ function gotStream(stream) {
 
   $('#controls').append($('<button id="record">Record</button>'));
   $('#record').click(function() {
-    options.video = video;
-    recordRTC = RecordRTC(stream, options);
-    recordRTC.startRecording();
-    $('#record').html('Stop');
-    $('#record').off('click');
-    $('#record').click(function () {
-      $('#sharing').css({display: 'block'});
-      recordRTC.stopRecording(function(videoWebURL) {
-        var recordedBlob = recordRTC.getBlob();
-        chrome.runtime.sendMessage({
-          'action': 'crop',
-          'url': videoWebURL,
-          'width': 1920,
-          'height': 1200,
-          'horizontal': 0,
-          'vertical': 0,
-        }, function(croppedURL) {
-          video.src = croppedURL;
-          video.controls = true;
-          video.loop = true;
-          
-        });
-        
-        // $('body').append($('<img/>'));
-        // $('img').src = videoWebURL;
-
-        recordRTC.getDataURL(function(dataURL) { });
-      });
-    });
+    record(video, stream);
   });
 }
 
@@ -91,10 +121,13 @@ function onAccessApproved(id) {
 var pending_request_id = null;
 
 $(document).ready(function() {
-  $('#start').on('click', function(e) {
+
+  function selectWindow(e) {
     pending_request_id = chrome.desktopCapture.chooseDesktopMedia(
         ["screen", "window"], onAccessApproved);
-  });
+  }
+
+  $('#start').on('click', selectWindow);
 
   $('#cancel').on('click', function(e) {
     if (pending_request_id !== null) {
