ad33a03693d0d14c9317a9694a5d0e34d51c56c3
dashbrown <dtb63@cornell.edu>
2015-09-19 20:03:46 -0400

    updated playing with sliders

--- sliders/slider.js
+++ sliders/slider.js
@@ -1,24 +1,39 @@
 $(document).ready(function() {
+  var i = 0;
+  var seconds = 500; // TODO: HOW DO WE GET THIS
   function addSlider() {
-    $( "#slider-range" ).slider({
+    $('#input').append($('<div id="slider-range' + i + '"></div><br>'));
+    $('#input').append($('<input id="sub' + i + '" type="text"></div><br><br>'));
+    $( "#slider-range"+i ).slider({
       range: true,
       min: 0,
-      max: 500,
-      values: [ 0, 500 ],
+      max: seconds,
+      values: [ 0, seconds ],
       slide: function( event, ui ) {
         $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
       }
     });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+    $( "#amount" ).val( "$" + $( "#slider-range"+i ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range"+i ).slider( "values", 1 ) );
+    i++;
   };
+
   addSlider();
-  $('button').click(function() {
-    console.log("clicked!");
-    // console.log($( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-    //   " - $" + $( "#slider-range" ).slider( "values", 1 )));
-    console.log($("#slider-range").slider("values")[0]);
-    console.log($("#slider-range").slider("values")[1]);
-    console.log($('#sub').val());
+  $('#add').click(function() {
+    console.log("Added!");
+    addSlider();
+  });
+
+  $('#check').click(function() {
+    // TODO
+    // send data to server for ffmpeg
+    console.log("Checking all sliders and text:");
+    for (var j = 0; j < i; j++) {
+      var str = "";
+      str += $("#slider-range"+j).slider("values")[0] + " ";
+      str += $("#slider-range"+j).slider("values")[1] + " ";
+      str += $("#sub"+j).val();
+      console.log(str);
+    }
   });
 });
